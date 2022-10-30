71ab57bda28366745e5f8624b6e8c5201942826d
Pedro Martins <pedrogmhead@gmail.com>
2022-10-30 00:41:37 -0300

    atualização da seção sobre mim

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,41 +1,22 @@
-<template>
-  <section>
-    <div class="container-fluid">
-      <div class="row justify-content-center">
-        <div class="col-12 text-center margin-top">
-          <h1>Sobre mim</h1>
-        </div>
-        <div class="col-12 col-md-7 mb-4 text-center">
-          <h2>
-            Se você está aqui é porque quer me conhecer melhor, então vamos lá!
-          </h2>
-          <div class="row justify-content-center">
-            <div class="col-10 col-md-8 col-lg-3">
-              <img
-                class="img-fluid my-picture"
-                src="@/assets/PedroMartins.jpg"
-              />
-            </div>
-            <div class="col-12 col-lg-9 ">
-              <p>
-                Estudante de Sistemas de Informação no IFSULDEMINAS - Campus
-                Machado. Trabalhando e aprimorando as soft e hard skills
-                diariamente, atualmente atuo como desenvolvedor full stack,
-                utilizando principalmente Java + Spring Boot nas tarefas
-                relacionadas ao backend, enquanto foco o desenvolvimento de um
-                bom front-end com o uso de VueJS
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+section.about
+  el-row(justify="center")
+    h1 Sobre mim
+  el-row(justify="center")
+    h2 Se você está aqui significa que quer me conhecer melhor, então vamos lá!
+  p 
+    | Estudante de Sistemas de Informação no IFSULDEMINAS - Campus
+    | Machado. Trabalhando e aprimorando as soft e hard skills
+    | diariamente, atualmente atuo como desenvolvedor full stack,
+    | utilizando principalmente Java + Spring Boot nas tarefas
+    | relacionadas ao backend, enquanto foco o desenvolvimento de um
+    | bom front-end com o uso de VueJS
 </template>
 <style scoped lang='scss'>
 @import "@/styles/variables.scss";
 
-.container-fluid {
+.about {
+  margin: -26px -8px 0 -8px;
   background-color: $-main-dark-ultra-light-gray;
 }
 
@@ -48,15 +29,6 @@ h2 {
   color: $-main-dark-heavy-blue;
   font-size: 2.4rem;
   font-weight: 600;
-}
-
-.margin-top {
-  margin-top: 7rem;
-}
-
-.my-picture {
-  border-radius: 20%;
-  margin-top: 5rem;
 }
 
 p {
