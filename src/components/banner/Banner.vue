7263c119601e649e257e1de9edc7c0f059e0d0f5
Pedro Martins <pedrogmhead@gmail.com>
2022-10-29 23:43:31 -0300

    Refatoração do banner

--- src/components/banner/Banner.vue
+++ src/components/banner/Banner.vue
@@ -1,22 +1,11 @@
-<template>
-<div class="banner">
-  <img
-    :src='bannerUrl'
-    alt='Banner da home'
+<template lang="pug">
+.banner
+  img.banner-img(
+    src='@/assets/banner.png',
+    alt='Banner da home',
     title='Banner da home'
-    class='banner-img'
-  >
-</div>
+  )
 </template>
-<script>
-  export default {
-    data() {
-      return {
-        bannerUrl: require('@/assets/banner.png')
-      };
-    },
-  }
-</script>
 <style scoped>
 .banner {
   margin: 8px -8px 0 -8px;
